28ac378a59214bf3cffd0ab12ee220db7ccbca59
J M <jin.ma@zstack.io>
2023-12-05 15:39:56 +0800

    <fix>[expon]: support mask sensitive info in query api
    rename VHost to Vhost
    
    Resolves: ZSTAC-60678
    
    Change-Id: I796c656568776a64636b6278696c66636b616d7a

--- plugin/kvm/src/main/java/org/zstack/kvm/VolumeTO.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/VolumeTO.java
@@ -54,7 +54,7 @@ public class VolumeTO extends BaseVirtualDeviceTO {
     private int controllerIndex;
 
     static {
-        deviceTypes.put(VolumeProtocol.VHost, VHOST);
+        deviceTypes.put(VolumeProtocol.Vhost, VHOST);
     }
 
     public VolumeTO() {
