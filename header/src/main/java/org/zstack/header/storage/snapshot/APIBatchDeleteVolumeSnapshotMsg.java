1f3535592720d125facf3a093a0b690a608510e1
tao.gan <tao.gan@zstack.io>
2024-02-05 16:43:59 +0800

    <fix>[volume]: online delete volume snapshot by block-commit
    APIImpact
    
    Resolves: ZSV-4606
    
    Change-Id: I6b72706a71796c646567686b746d756478656a78

--- header/src/main/java/org/zstack/header/storage/snapshot/APIBatchDeleteVolumeSnapshotMsg.java
+++ header/src/main/java/org/zstack/header/storage/snapshot/APIBatchDeleteVolumeSnapshotMsg.java
@@ -34,6 +34,9 @@ public class APIBatchDeleteVolumeSnapshotMsg extends APIDeleteMessage implements
     @APIParam(resourceType = VolumeSnapshotVO.class, successIfResourceNotExisting = true, checkAccount = true, nonempty = true, emptyString = false)
     private List<String> uuids;
 
+    @APIParam(required = false)
+    private boolean onlySelf = false;
+
     /**
      * @ignore
      */
@@ -54,6 +57,14 @@ public class APIBatchDeleteVolumeSnapshotMsg extends APIDeleteMessage implements
 
     public void setUuids(List<String> uuids) {
         this.uuids = uuids;
+    }
+
+    public boolean isOnlySelf() {
+        return onlySelf;
+    }
+
+    public void setOnlySelf(boolean onlySelf) {
+        this.onlySelf = onlySelf;
     }
 
     public static APIBatchDeleteVolumeSnapshotMsg __example__() {
