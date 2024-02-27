a64f3eb6cda06376b497ca67160e767a7d01a90d
wei.wang <wei.wang@zstack.io>
2024-02-27 13:14:25 +0800

    <feature>[conf]: test db upgrade
    Test db upgrade review
    
    DBImpact
    
    Resolves: ZSTAC-1234
    
    Change-Id: I676564706276727067697964796d61677374696e

---
+++ conf/db/upgrade/V5.0.1__schema.sql
@@ -0,0 +1,25 @@
+alter table SNSDingTalkEndpointVO
+    add secret1 varchar(128) default '' null;
+
+alter table SNSDingTalkAtPersonVO
+    add lastOpDate1 timestamp ON UPDATE CURRENT_TIMESTAMP;
+
+alter table SNSDingTalkAtPersonVO
+    add createDate1 timestamp NOT NULL DEFAULT '0000-00-00 00:00:00';
+
+alter table SNSDingTalkAtPersonVO
+    add remark1 varchar(128) default '' null;
+
+
+alter table SNSDingTalkEndpointVO
+    add secret2 varchar(128) default '' null;
+
+alter table SNSDingTalkAtPersonVO
+    add lastOpDate2 timestamp ON UPDATE CURRENT_TIMESTAMP;
+
+alter table SNSDingTalkAtPersonVO
+    add createDate2 timestamp NOT NULL DEFAULT '0000-00-00 00:00:00';
+
+alter table SNSDingTalkAtPersonVO
+    add remark2 varchar(128) default '' null;
+
