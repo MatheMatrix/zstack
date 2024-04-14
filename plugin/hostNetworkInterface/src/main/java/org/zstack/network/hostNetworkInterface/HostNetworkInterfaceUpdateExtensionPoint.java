fed65b814a35c66ff658928012415fd7d174dc0a
shixin.ruan <shixin.ruan@zstack.io>
2024-04-14 13:18:30 +0800

    <fix>[hostNetworkInterface]: remove unused code
    Resolves: ZSTAC-64939
    
    Change-Id: I6d726473687878676c62627774776d6c64666765

--- plugin/hostNetworkInterface/src/main/java/org/zstack/network/hostNetworkInterface/HostNetworkInterfaceUpdateExtensionPoint.java
+++
@@ -1,7 +0,0 @@
-package org.zstack.network.hostNetworkInterface;
-
-import java.util.List;
-
-public interface HostNetworkInterfaceUpdateExtensionPoint {
-    public void afterCreated(String hostUuid, List<HostNetworkInterfaceVO> interfaceVOS, List<HostNetworkBondingVO> bondingVOS);
-}
