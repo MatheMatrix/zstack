76040486e6c7dffcd47263f34023f9eb03626754
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-07-13 18:34:11 +0800

    <feature>[conf]: add SSO related database tables
    1. refactor: Create/Update/Delete OAuth2Client Action
    2. refactor: UpdateSSORedirectTemplate Action
    3. system tag "ssoUseAsLoginName" update to
       OAuth2AccountClientVO.usernameProperty
    
    Resolves: ZSV-5531
    
    Change-Id: I7172696a687a76796b6778696f736a686b6e7265

--- sdk/src/main/java/org/zstack/sdk/CreateOAuthClientResult.java
+++ sdk/src/main/java/org/zstack/sdk/CreateOAuthClientResult.java
@@ -1,13 +1,13 @@
 package org.zstack.sdk;
 
-import org.zstack.sdk.OAuth2ClientInventory;
+import org.zstack.sdk.OAuth2AccountClientInventory;
 
 public class CreateOAuthClientResult {
-    public OAuth2ClientInventory inventory;
-    public void setInventory(OAuth2ClientInventory inventory) {
+    public OAuth2AccountClientInventory inventory;
+    public void setInventory(OAuth2AccountClientInventory inventory) {
         this.inventory = inventory;
     }
-    public OAuth2ClientInventory getInventory() {
+    public OAuth2AccountClientInventory getInventory() {
         return this.inventory;
     }
 
---
+++ sdk/src/main/java/org/zstack/sdk/OAuth2AccountClientInventory.java
@@ -0,0 +1,159 @@
+package org.zstack.sdk;
+
+
+
+public class OAuth2AccountClientInventory  {
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    public java.lang.String description;
+    public void setDescription(java.lang.String description) {
+        this.description = description;
+    }
+    public java.lang.String getDescription() {
+        return this.description;
+    }
+
+    public java.lang.String type;
+    public void setType(java.lang.String type) {
+        this.type = type;
+    }
+    public java.lang.String getType() {
+        return this.type;
+    }
+
+    public java.lang.String createAccountStrategy;
+    public void setCreateAccountStrategy(java.lang.String createAccountStrategy) {
+        this.createAccountStrategy = createAccountStrategy;
+    }
+    public java.lang.String getCreateAccountStrategy() {
+        return this.createAccountStrategy;
+    }
+
+    public java.lang.String deleteAccountStrategy;
+    public void setDeleteAccountStrategy(java.lang.String deleteAccountStrategy) {
+        this.deleteAccountStrategy = deleteAccountStrategy;
+    }
+    public java.lang.String getDeleteAccountStrategy() {
+        return this.deleteAccountStrategy;
+    }
+
+    public java.sql.Timestamp createDate;
+    public void setCreateDate(java.sql.Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public java.sql.Timestamp getCreateDate() {
+        return this.createDate;
+    }
+
+    public java.sql.Timestamp lastOpDate;
+    public void setLastOpDate(java.sql.Timestamp lastOpDate) {
+        this.lastOpDate = lastOpDate;
+    }
+    public java.sql.Timestamp getLastOpDate() {
+        return this.lastOpDate;
+    }
+
+    public java.lang.String clientType;
+    public void setClientType(java.lang.String clientType) {
+        this.clientType = clientType;
+    }
+    public java.lang.String getClientType() {
+        return this.clientType;
+    }
+
+    public java.lang.String loginMNUrl;
+    public void setLoginMNUrl(java.lang.String loginMNUrl) {
+        this.loginMNUrl = loginMNUrl;
+    }
+    public java.lang.String getLoginMNUrl() {
+        return this.loginMNUrl;
+    }
+
+    public java.lang.String redirectUrl;
+    public void setRedirectUrl(java.lang.String redirectUrl) {
+        this.redirectUrl = redirectUrl;
+    }
+    public java.lang.String getRedirectUrl() {
+        return this.redirectUrl;
+    }
+
+    public java.lang.String clientId;
+    public void setClientId(java.lang.String clientId) {
+        this.clientId = clientId;
+    }
+    public java.lang.String getClientId() {
+        return this.clientId;
+    }
+
+    public java.lang.String clientSecret;
+    public void setClientSecret(java.lang.String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+    public java.lang.String getClientSecret() {
+        return this.clientSecret;
+    }
+
+    public java.lang.String authorizationUrl;
+    public void setAuthorizationUrl(java.lang.String authorizationUrl) {
+        this.authorizationUrl = authorizationUrl;
+    }
+    public java.lang.String getAuthorizationUrl() {
+        return this.authorizationUrl;
+    }
+
+    public java.lang.String tokenUrl;
+    public void setTokenUrl(java.lang.String tokenUrl) {
+        this.tokenUrl = tokenUrl;
+    }
+    public java.lang.String getTokenUrl() {
+        return this.tokenUrl;
+    }
+
+    public java.lang.String grantType;
+    public void setGrantType(java.lang.String grantType) {
+        this.grantType = grantType;
+    }
+    public java.lang.String getGrantType() {
+        return this.grantType;
+    }
+
+    public java.lang.String userinfoUrl;
+    public void setUserinfoUrl(java.lang.String userinfoUrl) {
+        this.userinfoUrl = userinfoUrl;
+    }
+    public java.lang.String getUserinfoUrl() {
+        return this.userinfoUrl;
+    }
+
+    public java.lang.String logoutUrl;
+    public void setLogoutUrl(java.lang.String logoutUrl) {
+        this.logoutUrl = logoutUrl;
+    }
+    public java.lang.String getLogoutUrl() {
+        return this.logoutUrl;
+    }
+
+    public java.lang.String usernameProperty;
+    public void setUsernameProperty(java.lang.String usernameProperty) {
+        this.usernameProperty = usernameProperty;
+    }
+    public java.lang.String getUsernameProperty() {
+        return this.usernameProperty;
+    }
+
+}
