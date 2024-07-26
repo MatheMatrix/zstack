06b55cf6901e610a1095b46d56f8e5e07b0fff92
hanyu.liang <hanyu.liang@zstack.io>
2024-07-26 18:14:16 +0800

    <feature>[crypto]: support SanSec Secret resource
    Resolves: ZSTAC-67973
    
    Change-Id: I7778676171646874706164777869707679736172

--- conf/db/upgrade/V4.4.64__schema.sql
+++ conf/db/upgrade/V4.4.64__schema.sql
@@ -1,5 +1,19 @@
 UPDATE ResourceConfigVO SET createDate = CURRENT_TIMESTAMP where name='iam2.force.enable.securityGroup' and createDate='0000-00-00 00:00:00';
 
+CREATE TABLE IF NOT EXISTS `zstack`.`SanSecSecretResourcePoolVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `keyIndex` varchar(128) DEFAULT NULL,
+    PRIMARY KEY  (`uuid`),
+    CONSTRAINT fkSanSecSecretResourcePoolVOSecretResourcePoolVO FOREIGN KEY (uuid) REFERENCES SecretResourcePoolVO (uuid) ON UPDATE RESTRICT ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `zstack`.`SanSecSecretResourcePoolVO` ADD COLUMN `managementIp` varchar(128) DEFAULT NULL;
+ALTER TABLE `zstack`.`SanSecSecretResourcePoolVO` ADD COLUMN `port` int unsigned DEFAULT NULL;
+ALTER TABLE `zstack`.`SanSecSecretResourcePoolVO` ADD COLUMN `username` varchar(128) DEFAULT NULL;
+ALTER TABLE `zstack`.`SanSecSecretResourcePoolVO` ADD COLUMN `password` varchar(128) DEFAULT NULL;
+ALTER TABLE `zstack`.`SanSecSecretResourcePoolVO` ADD COLUMN `sm3Key` varchar(128) DEFAULT NULL;
+ALTER TABLE `zstack`.`SanSecSecretResourcePoolVO` ADD COLUMN `sm4Key` varchar(128) DEFAULT NULL;
+
 DROP PROCEDURE IF EXISTS AddFkPciDeviceVOVmInstanceEO;
 DELIMITER $$
 CREATE PROCEDURE AddFkPciDeviceVOVmInstanceEO()
