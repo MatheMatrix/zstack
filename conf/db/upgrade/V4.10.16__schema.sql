ccb44d4b6bde1c6c16570233e3e86096e25c9618
lin.ma <lin.ma@zstack.io>
2024-08-16 14:22:43 +0800

    <feature>[guesttools]: Added the VM script
    1. Add the VM script collection management function
    2. Add the batch command execution function
    3. Add the file upload function
    
    Resolves: ZSV-9007
    Resolves: ZSTAC-56790
    
    Change-Id: I656f796f717a6761756f737868787163616b616a

--- conf/db/upgrade/V4.10.16__schema.sql
+++ conf/db/upgrade/V4.10.16__schema.sql
@@ -27,3 +27,57 @@ CREATE TABLE IF NOT EXISTS `zstack`.`VolumeCbtBackupRecordVO` (
     `createDate` timestamp NOT NULL DEFAULT '1999-12-31 23:59:59',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `zstack`.`GuestVmScriptEO` (
+    `uuid` VARCHAR(32) NOT NULL UNIQUE,
+    `name` VARCHAR(256) NOT NULL,
+    `description` VARCHAR(256),
+    `platform` VARCHAR(255) NOT NULL,
+    `scriptContent` MEDIUMTEXT,
+    `renderParams` MEDIUMTEXT,
+    `scriptType` VARCHAR(32) NOT NULL,
+    `scriptTimeout` INT UNSIGNED NOT NULL,
+    `version` INT UNSIGNED NOT NULL,
+    `deleted` VARCHAR(255) DEFAULT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '1999-12-31 23:59:59' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '1999-12-31 23:59:59',
+    PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP VIEW IF EXISTS `zstack`.`GuestVmScriptVO`;
+CREATE VIEW `zstack`.`GuestVmScriptVO` AS SELECT uuid, name, description, platform, scriptContent, renderParams, scriptType, scriptTimeout, version, createDate, lastOpDate FROM `zstack`.`GuestVmScriptEO` WHERE deleted IS NULL;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`GuestVmScriptExecutedRecordVO` (
+    `uuid` VARCHAR(32) NOT NULL UNIQUE,
+    `recordName` VARCHAR(255) NOT NULL,
+    `scriptUuid` VARCHAR(32) NOT NULL,
+    `scriptTimeout` INT UNSIGNED NOT NULL,
+    `status` VARCHAR(256) NOT NULL,
+    `version` INT UNSIGNED NOT NULL,
+    `Executor` VARCHAR(256) NOT NULL ,
+    `ExecutionCount` INT UNSIGNED NOT NULL,
+    `scriptContent` MEDIUMTEXT,
+    `renderParams` MEDIUMTEXT,
+    `startTime` TIMESTAMP NOT NULL DEFAULT '1999-12-31 23:59:59',
+    `endTime` TIMESTAMP NOT NULL DEFAULT '1999-12-31 23:59:59',
+    PRIMARY KEY (`uuid`),
+    INDEX `idxScriptUuid` (`scriptUuid`, `version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`GuestVmScriptExecutedRecordDetailVO` (
+    `recordUuid` VARCHAR(32) NOT NULL,
+    `vmInstanceUuid` VARCHAR(32) NOT NULL,
+    `vmName` VARCHAR(255) NOT NULL,
+    `status` VARCHAR(128) NOT NULL,
+    `exitCode` INT UNSIGNED,
+    `stdout` MEDIUMTEXT,
+    `errCause` MEDIUMTEXT,
+    `stderr` MEDIUMTEXT,
+    `startTime` TIMESTAMP NOT NULL DEFAULT '1999-12-31 23:59:59',
+    `endTime` TIMESTAMP NOT NULL DEFAULT '1999-12-31 23:59:59',
+    PRIMARY KEY (`recordUuid`, `vmInstanceUuid`),
+    CONSTRAINT `fkGuestVmScriptExecutedRecordDetailVOScriptExecutedRecordVO` FOREIGN KEY (`recordUuid`) REFERENCES `GuestVmScriptExecutedRecordVO` (`uuid`) ON DELETE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `zstack`.`AuditsVO` MODIFY COLUMN requestDump MEDIUMTEXT, MODIFY COLUMN responseDump MEDIUMTEXT;
