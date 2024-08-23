c42fe7a5e5dff3009f0a132d441dd36212ad5ad4
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-08-23 17:51:02 +0800

    <feature>[identity]: update RBACAPIRequestChecker
    * RBACAPIRequestChecker will check account role policies
    
    In most cases, only two requirements are needed for
    an account to call APIs:
    - having API permissions
    - being able to access resources.
    
    This condition is completely different from Cloud products.
    Cloud products only allow **owners** to execute most of the APIs.
    
    Based on this requirement, RBAC.targetResources method
    has been officially deprecated.
    
    Resolves: ZSV-6705
    Related: ZSV-6559
    
    Change-Id: I6f797a79726f77616d66747971716d736a786375

--- identity/src/main/java/org/zstack/identity/rbac/PolicyUtils.java
+++ identity/src/main/java/org/zstack/identity/rbac/PolicyUtils.java
@@ -2,6 +2,10 @@ package org.zstack.identity.rbac;
 
 import org.zstack.header.identity.rbac.RBAC;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PolicyUtils {
     public static boolean isAdminOnlyAction(String action) {
         return RBAC.isAdminOnlyAPI(apiNamePatternFromAction(action));
@@ -23,5 +27,30 @@ public class PolicyUtils {
         } else {
             return splited[1];
         }
+    }
+
+    /**
+     * Example:<br/>
+     *
+     * input: "org.zstack.header.vm.APIStartVmInstanceMsg"<br/>
+     * output [".**", ".header.**", ".header.vm.**", ".header.vm.*", ".header.vm.APIStartVmInstanceMsg"]<br/>
+     */
+    public static List<String> findAllMatchedApiPatterns(String api) {
+        List<String> results = new ArrayList<>();
+
+        if (!api.startsWith("org.zstack.")) {
+            results.add(api);
+            return results;
+        }
+
+        api = api.substring("org.zstack.".length()); // start without "."
+        String[] split = api.split("\\.");
+        results.add(".**");
+        for (int i = 1; i < split.length; i++) {
+            results.add("." + String.join(".", Arrays.copyOfRange(split, 0, i)) + ".**");
+        }
+        results.add("." + String.join(".", Arrays.copyOfRange(split, 0, split.length - 1)) + ".*");
+        results.add("." + api);
+        return results;
     }
 }
