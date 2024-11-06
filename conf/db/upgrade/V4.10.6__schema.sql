67d590c96a6886baeaa61b036b6f51723bdad0c6
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-11-06 15:19:56 +0800

    <feature>[conf]: introduce zstone-plugin
    APIImpact
    DBImpact
    
    Resolves: ZSV-7443
    
    Change-Id: I69626a6c6579746d6d7a79776665737168747169

--- conf/db/upgrade/V4.10.6__schema.sql
+++ conf/db/upgrade/V4.10.6__schema.sql
@@ -1,3 +1,21 @@
+-- Feature: ZStone Support | ZSV-7443
+
+create table if not exists `zstack`.`ZStoneVO` (
+    `uuid` char(32) not null unique,
+    `name` varchar(255) not null,
+    `username` varchar(255) not null,
+    `managementIp` varchar(255) not null,
+    `authorizationServer` varchar(32) not null,
+    `logInPort` int(11) not null,
+    `apiPort` int(11) not null,
+    `logInUrl` varchar(32) not null,
+    `lastOpDate` timestamp on update CURRENT_TIMESTAMP,
+    `createDate` timestamp,
+    primary key (`uuid`)
+) ENGINE=InnoDB default CHARSET=utf8;
+
+-- Feature: Storage Adapters | ZSV-7389
+
 CREATE TABLE IF NOT EXISTS `zstack`.`HbaDeviceVO` (
     `uuid` varchar(32) not null unique,
     `hostUuid` varchar(32) default null,
---
+++ sdk/src/main/java/org/zstack/sdk/zstone/entity/ZStoneInventory.java
@@ -0,0 +1,87 @@
+package org.zstack.sdk.zstone.entity;
+
+
+
+public class ZStoneInventory  {
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    public java.lang.String username;
+    public void setUsername(java.lang.String username) {
+        this.username = username;
+    }
+    public java.lang.String getUsername() {
+        return this.username;
+    }
+
+    public java.lang.String managementIp;
+    public void setManagementIp(java.lang.String managementIp) {
+        this.managementIp = managementIp;
+    }
+    public java.lang.String getManagementIp() {
+        return this.managementIp;
+    }
+
+    public java.lang.String authorizationServer;
+    public void setAuthorizationServer(java.lang.String authorizationServer) {
+        this.authorizationServer = authorizationServer;
+    }
+    public java.lang.String getAuthorizationServer() {
+        return this.authorizationServer;
+    }
+
+    public int logInPort;
+    public void setLogInPort(int logInPort) {
+        this.logInPort = logInPort;
+    }
+    public int getLogInPort() {
+        return this.logInPort;
+    }
+
+    public int apiPort;
+    public void setApiPort(int apiPort) {
+        this.apiPort = apiPort;
+    }
+    public int getApiPort() {
+        return this.apiPort;
+    }
+
+    public java.lang.String logInUrl;
+    public void setLogInUrl(java.lang.String logInUrl) {
+        this.logInUrl = logInUrl;
+    }
+    public java.lang.String getLogInUrl() {
+        return this.logInUrl;
+    }
+
+    public java.sql.Timestamp createDate;
+    public void setCreateDate(java.sql.Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public java.sql.Timestamp getCreateDate() {
+        return this.createDate;
+    }
+
+    public java.sql.Timestamp lastOpDate;
+    public void setLastOpDate(java.sql.Timestamp lastOpDate) {
+        this.lastOpDate = lastOpDate;
+    }
+    public java.sql.Timestamp getLastOpDate() {
+        return this.lastOpDate;
+    }
+
+}
