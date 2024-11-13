19b9d1bb7df928fee41cb9f60b32e6827f01e10b
Zhang, Wenhao <wenhao.zhang@zstack.io>
2024-11-13 10:45:40 +0000

    <feature>[sdk]: support update ZStone hosts and get ZStone capability
    When adding ZStone hosts, the first 3 hosts are configured
    as ZStone management nodes by default, and the remaining
    hosts are configured as storage nodes.
    
    Note: **Incomplete Patch**
    
    Because ZStone supports ZBS clusters,
    the subsequent ZStone capabilities query will differentiate
    the URLs searched based on the type of cluster.
    
    Resolves: ZSV-7443
    
    Change-Id: I70656e796f77686d79766a79646b6d6d696f6a62

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -44164,6 +44164,33 @@ abstract class ApiHelper {
     }
 
 
+    def getZStoneCapability(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.zstone.api.GetZStoneCapabilityAction.class) Closure c) {
+        def a = new org.zstack.sdk.zstone.api.GetZStoneCapabilityAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def queryZStone(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.zstone.api.QueryZStoneAction.class) Closure c) {
         def a = new org.zstack.sdk.zstone.api.QueryZStoneAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
@@ -44222,6 +44249,33 @@ abstract class ApiHelper {
 
     def updateZStoneClusterConfig(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.zstone.api.UpdateZStoneClusterConfigAction.class) Closure c) {
         def a = new org.zstack.sdk.zstone.api.UpdateZStoneClusterConfigAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
+    def updateZStoneHostConfig(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.zstone.api.UpdateZStoneHostConfigAction.class) Closure c) {
+        def a = new org.zstack.sdk.zstone.api.UpdateZStoneHostConfigAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
         c.resolveStrategy = Closure.OWNER_FIRST
         c.delegate = a
