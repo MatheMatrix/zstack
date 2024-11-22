bcfa1ae4b6e8d5db9e4f92ea37517874589973c2
AlanJager <ye.zou@zstack.io>
2024-11-22 17:30:14 +0800

    <fix>[conf]: Fix AutoScalingRuleSchedulerJobTriggerVO deletion issue
    - AutoScalingSchedulerJobCase clean env may failed due to fk error
      because jpa mark the column with deletion cascade but sql do not
    
    DBImpact
    
    Resolves: ZSTAC-68892
    
    Change-Id: I64766a6c626c6b797872636b6677686e71657175

--- conf/db/upgrade/V5.3.0__schema.sql
+++ conf/db/upgrade/V5.3.0__schema.sql
@@ -51,3 +51,6 @@ INSERT IGNORE INTO `zstack`.`HostNetworkLabelVO` (`uuid`, `serviceType`, `system
     VALUES (REPLACE(UUID(),'-',''),'MigrationNetwork', TRUE, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 CREATE INDEX idx_schedType_createDate ON `zstack`.`VmSchedHistoryVO` (schedType, createDate);
+
+ALTER TABLE AutoScalingRuleSchedulerJobTriggerVO DROP FOREIGN KEY fkAutoScalingRuleSchedulerJobTriggerVO;
+CALL ADD_CONSTRAINT('AutoScalingRuleSchedulerJobTriggerVO', 'fkAutoScalingRuleSchedulerJobTriggerVO', 'schedulerJobUuid', 'SchedulerJobVO', 'uuid', 'CASCADE');
