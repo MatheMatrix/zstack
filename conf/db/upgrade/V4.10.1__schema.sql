55b03a3a95dca874684c5c286373cdf2640371a0
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-11-27 10:20:49 +0800

    <fix>[conf]: remove deleted entity in table EncryptEntityMetadataVO
    "APP Center", "IAM2" are not supported in ZSphere
    
    Resolves: ZSV-7626
    
    DBImpact
    
    Change-Id: I6a6e77727a706d616e6479736367617069777477

--- conf/db/upgrade/V4.10.1__schema.sql
+++ conf/db/upgrade/V4.10.1__schema.sql
@@ -1,1 +1,3 @@
 CALL INSERT_COLUMN('L2NetworkHostRefVO', 'skipDeletion', 'boolean', 0, false, 'bridgeName');
+
+delete from `EncryptEntityMetadataVO` where `entityName` in ('AppBuildSystemVO', 'IAM2VirtualIDAttributeVO');
