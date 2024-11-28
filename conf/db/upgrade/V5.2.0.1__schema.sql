1f016c2158c5fb6060aeb321c73da9183e232ecf
xinhao.huang <xinhao.huang@zstack.io>
2024-11-28 16:10:51 +0800

    <feature>[db]: fix unstable case AutoScalingSchedulerJobCase
    - Added cascade behavior to foreign keys in `AutoScalingRuleSchedulerJobTriggerVO` and `SchedulerJobVO` to ensure referential integrity and simplify cleanup operations.
    - Created a composite index on `schedType` and `createDate` in `VmSchedHistoryVO` to improve query performance for scheduling-related operations.
    
    DBImpact
    
    Resolves: ZHCI-2914
    
    Change-Id: I72687367656a6273737475656d67696f696e7662

---
+++ conf/db/upgrade/V5.2.0.1__schema.sql
@@ -0,0 +1,4 @@
+CREATE INDEX idx_schedType_createDate ON `zstack`.`VmSchedHistoryVO` (schedType, createDate);
+
+ALTER TABLE AutoScalingRuleSchedulerJobTriggerVO DROP FOREIGN KEY fkAutoScalingRuleSchedulerJobTriggerVO;
+CALL ADD_CONSTRAINT('AutoScalingRuleSchedulerJobTriggerVO', 'fkAutoScalingRuleSchedulerJobTriggerVO', 'schedulerJobUuid', 'SchedulerJobVO', 'uuid', 'CASCADE');
