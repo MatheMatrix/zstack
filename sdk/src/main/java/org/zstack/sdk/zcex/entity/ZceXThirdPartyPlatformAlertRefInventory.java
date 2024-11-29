e1ee33d9163dd4838644d761f5285a99a9bc422b
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-11-29 16:03:14 +0800

    <feature>[sdk]: introduce ZCE-X alarm service
    Support fetching ZCE-X alarm information
    through "zwatch.ThirdpartyPlatformVO"
    and sending it to SNS services.
    
    Add ZCE-X alarm related error codes
    
    Resolves: ZSV-7444
    
    DBImpact
    APIImpact
    
    Change-Id: I6b697364766c6e6b79746963616567686e676361

---
+++ sdk/src/main/java/org/zstack/sdk/zcex/entity/ZceXThirdPartyPlatformAlertRefInventory.java
@@ -0,0 +1,31 @@
+package org.zstack.sdk.zcex.entity;
+
+
+
+public class ZceXThirdPartyPlatformAlertRefInventory  {
+
+    public java.lang.String zceXUuid;
+    public void setZceXUuid(java.lang.String zceXUuid) {
+        this.zceXUuid = zceXUuid;
+    }
+    public java.lang.String getZceXUuid() {
+        return this.zceXUuid;
+    }
+
+    public java.lang.String thirdPartyPlatformUuid;
+    public void setThirdPartyPlatformUuid(java.lang.String thirdPartyPlatformUuid) {
+        this.thirdPartyPlatformUuid = thirdPartyPlatformUuid;
+    }
+    public java.lang.String getThirdPartyPlatformUuid() {
+        return this.thirdPartyPlatformUuid;
+    }
+
+    public java.sql.Timestamp createDate;
+    public void setCreateDate(java.sql.Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public java.sql.Timestamp getCreateDate() {
+        return this.createDate;
+    }
+
+}
