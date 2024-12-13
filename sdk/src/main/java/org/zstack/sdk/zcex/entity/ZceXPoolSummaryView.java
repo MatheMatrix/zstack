badcf9aa3b3d2ddeff7d759e6fffb150ab5b5746
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-12-13 17:29:29 +0800

    <fix>[zstone]: add pool name in ZStone and ZCE-X capability results
    Resolves: ZSV-7731
    Related: ZSV-7444
    
    Change-Id: I666675776665676d67656466736e716871676378

--- sdk/src/main/java/org/zstack/sdk/zcex/entity/ZceXPoolSummaryView.java
+++ sdk/src/main/java/org/zstack/sdk/zcex/entity/ZceXPoolSummaryView.java
@@ -20,4 +20,12 @@ public class ZceXPoolSummaryView  {
         return this.totalCapacity;
     }
 
+    public java.util.List inventories;
+    public void setInventories(java.util.List inventories) {
+        this.inventories = inventories;
+    }
+    public java.util.List getInventories() {
+        return this.inventories;
+    }
+
 }
