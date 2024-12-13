07a84ab78159a669156b223ea7ad3b64c2148ca0
AlanJager <ye.zou@zstack.io>
2024-12-13 18:57:12 +0800

    <chore>[sdk]: Update sdk
    Resolves: ZSTAC-71880
    
    Change-Id: I656770716166616c6a637863636779696d687269

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -13274,6 +13274,33 @@ abstract class ApiHelper {
     }
 
 
+    def deleteContainerResourceFromEndpoint(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.DeleteContainerResourceFromEndpointAction.class) Closure c) {
+        def a = new org.zstack.sdk.DeleteContainerResourceFromEndpointAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def deleteDataCenterInLocal(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.DeleteDataCenterInLocalAction.class) Closure c) {
         def a = new org.zstack.sdk.DeleteDataCenterInLocalAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
