2d97230a0277111a1cc70e79fd7c80651a0d4ca2
AlanJager <ye.zou@zstack.io>
2024-12-23 09:39:55 +0800

    <fix>[conf]: Add default system tags for system datasets
    - Update sdk and api helper
    - Update schema to support upgrade
    
    DBImpact
    
    Resolves: ZSTAC-70828
    
    Change-Id: I7164697774646a6d71616e61797a6b636868726f

--- conf/db/upgrade/V5.3.6__schema.sql
+++ conf/db/upgrade/V5.3.6__schema.sql
@@ -11,3 +11,31 @@ ALTER TABLE `zstack`.`ModelEvaluationTaskVO` ADD taskRequestInJson VARCHAR(8192)
 ALTER TABLE `zstack`.`ModelEvaluationTaskVO` ADD type VARCHAR(32) DEFAULT NULL;
 ALTER TABLE `zstack`.`ModelEvaluationTaskVO` MODIFY `evaluatedServiceGroupUuid` varchar(32) DEFAULT NULL;
 ALTER TABLE `zstack`.`ModelEvaluationTaskVO` MODIFY datasetUuid VARCHAR(32) DEFAULT NULL;
+
+INSERT INTO SystemTagVO
+(`uuid`, `resourceUuid`, `resourceType`, `inherent`, `type`, `tag`, `createDate`, `lastOpDate`)
+SELECT
+    REPLACE(UUID(), '-', ''),                  -- 生成不含连字符的uuid
+    uuid,                                       -- 使用DatasetVO的uuid作为resourceUuid
+    'DatasetVO',                               -- resourceType
+    1,                                         -- inherent
+    'System',                                  -- type
+    'dataset::usage::scenarios::ModelEval',    -- tag
+    CURRENT_TIMESTAMP(),                       -- createDate
+    CURRENT_TIMESTAMP()                        -- lastOpDate
+FROM DatasetVO
+WHERE system = true;
+
+INSERT INTO SystemTagVO
+(`uuid`, `resourceUuid`, `resourceType`, `inherent`, `type`, `tag`, `createDate`, `lastOpDate`)
+SELECT
+    REPLACE(UUID(), '-', ''),                  -- 生成不含连字符的uuid
+    uuid,                                       -- 使用DatasetVO的uuid作为resourceUuid
+    'DatasetVO',                               -- resourceType
+    1,                                         -- inherent
+    'System',                                  -- type
+    'dataset::datatype::Text',                 -- tag
+    CURRENT_TIMESTAMP(),                       -- createDate
+    CURRENT_TIMESTAMP()                        -- lastOpDate
+FROM DatasetVO
+WHERE system = true;
