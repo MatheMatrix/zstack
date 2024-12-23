b9eafc1a1f79fa867183dee1adaf377e2e294de4
AlanJager <ye.zou@zstack.io>
2024-12-23 09:39:55 +0800

    <fix>[conf]: Add default system tags for system datasets
    - Update sdk and api helper
    - Update schema to support upgrade
    
    DBImpact
    
    Resolves: ZSTAC-70828
    
    Change-Id: I7164697774646a6d71616e61797a6b636868726f

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -13355,6 +13355,33 @@ abstract class ApiHelper {
     }
 
 
+    def deleteDatasets(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.DeleteDatasetsAction.class) Closure c) {
+        def a = new org.zstack.sdk.DeleteDatasetsAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def deleteDirectory(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.DeleteDirectoryAction.class) Closure c) {
         def a = new org.zstack.sdk.DeleteDirectoryAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
