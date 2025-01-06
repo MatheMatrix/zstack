295b83aca6be0589ce08c52be4b6301424239b02
AlanJager <ye.zou@zstack.io>
2025-01-06 15:40:14 +0800

    <chore>[sdk]: Update sdk
    Resolves: ZSTAC-72314
    
    Change-Id: I776c656865657673726d6e73746a6b726675776a

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -14921,6 +14921,33 @@ abstract class ApiHelper {
     }
 
 
+    def deleteModelEvaluationTasks(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.DeleteModelEvaluationTasksAction.class) Closure c) {
+        def a = new org.zstack.sdk.DeleteModelEvaluationTasksAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def deleteModelService(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.DeleteModelServiceAction.class) Closure c) {
         def a = new org.zstack.sdk.DeleteModelServiceAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
@@ -42863,7 +42890,7 @@ abstract class ApiHelper {
         c.resolveStrategy = Closure.OWNER_FIRST
         c.delegate = a
         c()
-
+        
 
         if (System.getProperty("apipath") != null) {
             if (a.apiId == null) {
