72c3f039ee28be49b2013fc8393f0836f6885446
tao.yang <tao.yang@zstack.io>
2025-01-16 11:22:13 +0800

    <fix>[network]: adapt to get physical interface of vSwitch when vm ha
    Resolves: ZSV-7650
    
    Change-Id: I7a6c76656b6a6b74667564626a6a797561637974

--- header/src/main/java/org/zstack/header/network/l2/L2NetworkGetInterfaceExtensionPoint.java
+++ header/src/main/java/org/zstack/header/network/l2/L2NetworkGetInterfaceExtensionPoint.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface L2NetworkGetInterfaceExtensionPoint {
-    default List<String> getPhysicalInterfaceNames(String l2NetworkUuid, String hostUuid) {
+    default List<String> getHostNetworkInterfaceNames(String l2NetworkUuid, String hostUuid) {
         return new ArrayList<>();
+    }
+    default String getPhysicalInterfaceName(String l2NetworkUuid, String hostUuid) {
+        return null;
     }
     L2NetworkType getType();
     default boolean isPhysicalInterfaceOccupied(String interfaceName, String hostUuid) {
