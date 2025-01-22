30c14a0ec373c5169dfe4cb60e4a7d1f8acf7f27
Zhang Wenhao <wenhao.zhang@zstack.io>
2025-01-22 11:01:49 +0800

    <feature>[sdk]: added ZStone license information
    rename ZStonePlatformLicenseView to ZStoneLicenseInventory
    
    Resolves: ZSV-8021
    Related: ZSV-7443
    
    Change-Id: I766f756977676a7a72667a6b706f75646170656c

---
+++ sdk/src/main/java/org/zstack/sdk/zstone/entity/ZStoneLicenseInventory.java
@@ -0,0 +1,111 @@
+package org.zstack.sdk.zstone.entity;
+
+
+
+public class ZStoneLicenseInventory  {
+
+    public java.lang.String issuedTime;
+    public void setIssuedTime(java.lang.String issuedTime) {
+        this.issuedTime = issuedTime;
+    }
+    public java.lang.String getIssuedTime() {
+        return this.issuedTime;
+    }
+
+    public java.lang.String expiredTime;
+    public void setExpiredTime(java.lang.String expiredTime) {
+        this.expiredTime = expiredTime;
+    }
+    public java.lang.String getExpiredTime() {
+        return this.expiredTime;
+    }
+
+    public java.lang.Boolean expired;
+    public void setExpired(java.lang.Boolean expired) {
+        this.expired = expired;
+    }
+    public java.lang.Boolean getExpired() {
+        return this.expired;
+    }
+
+    public java.lang.String licenseType;
+    public void setLicenseType(java.lang.String licenseType) {
+        this.licenseType = licenseType;
+    }
+    public java.lang.String getLicenseType() {
+        return this.licenseType;
+    }
+
+    public java.lang.String prodInfo;
+    public void setProdInfo(java.lang.String prodInfo) {
+        this.prodInfo = prodInfo;
+    }
+    public java.lang.String getProdInfo() {
+        return this.prodInfo;
+    }
+
+    public java.lang.String productVersion;
+    public void setProductVersion(java.lang.String productVersion) {
+        this.productVersion = productVersion;
+    }
+    public java.lang.String getProductVersion() {
+        return this.productVersion;
+    }
+
+    public java.lang.String licenseAttr;
+    public void setLicenseAttr(java.lang.String licenseAttr) {
+        this.licenseAttr = licenseAttr;
+    }
+    public java.lang.String getLicenseAttr() {
+        return this.licenseAttr;
+    }
+
+    public java.lang.Long cpuNum;
+    public void setCpuNum(java.lang.Long cpuNum) {
+        this.cpuNum = cpuNum;
+    }
+    public java.lang.Long getCpuNum() {
+        return this.cpuNum;
+    }
+
+    public java.lang.Long usedCpuNum;
+    public void setUsedCpuNum(java.lang.Long usedCpuNum) {
+        this.usedCpuNum = usedCpuNum;
+    }
+    public java.lang.Long getUsedCpuNum() {
+        return this.usedCpuNum;
+    }
+
+    public java.lang.Long hostNum;
+    public void setHostNum(java.lang.Long hostNum) {
+        this.hostNum = hostNum;
+    }
+    public java.lang.Long getHostNum() {
+        return this.hostNum;
+    }
+
+    public java.lang.Long usedHostNum;
+    public void setUsedHostNum(java.lang.Long usedHostNum) {
+        this.usedHostNum = usedHostNum;
+    }
+    public java.lang.Long getUsedHostNum() {
+        return this.usedHostNum;
+    }
+
+    public java.lang.Long capacity;
+    public void setCapacity(java.lang.Long capacity) {
+        this.capacity = capacity;
+    }
+    public java.lang.Long getCapacity() {
+        return this.capacity;
+    }
+
+    public java.lang.Long usedCapacity;
+    public void setUsedCapacity(java.lang.Long usedCapacity) {
+        this.usedCapacity = usedCapacity;
+    }
+    public java.lang.Long getUsedCapacity() {
+        return this.usedCapacity;
+    }
+
+}
