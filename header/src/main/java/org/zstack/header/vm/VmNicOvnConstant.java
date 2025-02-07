9f4330f61620fcf73619ebb9d1a9ab7828c4c8e6
shixin.ruan <shixin.ruan@zstack.io>
2025-02-07 14:06:32 +0800

    <fix>[kvm]: sync ovn dpdk nic when reconnecting host<description>
    Resolves: ZSTAC-72911
    
    Change-Id: I656a7462706179647770716e71676a7067637476

--- header/src/main/java/org/zstack/header/vm/VmNicOvnConstant.java
+++
@@ -1,8 +0,0 @@
-package org.zstack.header.vm;
-
-import org.zstack.header.configuration.PythonClass;
-
-@PythonClass
-public interface VmNicOvnConstant {
-    public static final String OVN_DPDK_VNIC_TYPE_VHOST_USER = "OvnDpdkVhostUser";
-}
