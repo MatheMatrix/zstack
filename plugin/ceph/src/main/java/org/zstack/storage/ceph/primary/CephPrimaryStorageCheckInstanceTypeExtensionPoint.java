fa0e7eeccbcc47d0355896f7e5352f7e89729c72
shenjin <jin.shen@zstack.io>
2025-03-07 14:27:13 +0800

    <fix>[ceph]: fix delete bm root volume
    Resolves: ZSTAC-73396
    
    Change-Id: I6a6161787279726f766a636c6170666e6a72726a

--- plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephPrimaryStorageCheckInstanceTypeExtensionPoint.java
+++ plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephPrimaryStorageCheckInstanceTypeExtensionPoint.java
@@ -6,4 +6,6 @@ public interface CephPrimaryStorageCheckInstanceTypeExtensionPoint {
     Boolean isSupportCloneByThirdParty(String uuid);
 
     void convertToBlockVolume(VolumeVO vo);
+
+    Boolean isBlockVolume(String uuid);
 }
