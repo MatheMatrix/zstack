1acfd2c4a9d08e6b6e112d15239727024e52e6ab
wei.wang <wei.wang@zstack.io>
2025-04-08 18:31:33 +0800

    fix msg test

--- header/src/main/java/org/zstack/header/network/l3/APIAddIpv6RangeByNetworkCidr2Msg.java
+++ header/src/main/java/org/zstack/header/network/l3/APIAddIpv6RangeByNetworkCidr2Msg.java
@@ -17,7 +17,7 @@ import org.zstack.utils.network.IPv6Constants;
 @Action(category = L3NetworkConstant.ACTION_CATEGORY)
 @RestRequest(
         path = "/l3-networks/{l3NetworkUuid}/ipv6-ranges/by-cidr",
-        method = HttpMethod.PUT,
+        method = HttpMethod.DELETE,
         parameterName = "params",
         responseClass = APIAddIpRangeByNetworkCidr2Event.class
 )
