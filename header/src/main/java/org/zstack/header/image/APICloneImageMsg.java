7245a86c47cd3dbb6f25d47ad2536fa35dc1f9ce
yingzhe.hu <yingzhe.hu@zstack.io>
2025-05-30 17:45:40 +0800

    <feature>[image]: support image clone
    APIImpact
    
    Resolves: ZSTAC-75312
    
    Change-Id: I6674796875667a6a626865716b7866747978646f

---
+++ header/src/main/java/org/zstack/header/image/APICloneImageMsg.java
@@ -0,0 +1,56 @@
+package org.zstack.header.image;
+
+import org.springframework.http.HttpMethod;
+import org.zstack.header.identity.Action;
+import org.zstack.header.message.*;
+import org.zstack.header.other.APIAuditor;
+import org.zstack.header.rest.RestRequest;
+import org.zstack.header.tag.TagResourceType;
+
+@TagResourceType(ImageVO.class)
+@Action(category = ImageConstant.ACTION_CATEGORY)
+@RestRequest(
+        path = "/image/clone/{imageUuid}",
+        method = HttpMethod.POST,
+        parameterName = "params",
+        responseClass = APICloneImageEvent.class
+)
+public class APICloneImageMsg extends APICreateMessage implements APIAuditor {
+    @APIParam(resourceType = ImageVO.class, noOwnerCheck = true)
+    private String imageUuid;
+
+    @APIParam(resourceType = ImageVO.class, noOwnerCheck = true)
+    private String imageGroupUuid;
+
+    @APIParam(required = false, validValues = {"DatabaseOnly"})
+    private String strategy = ImageCloneStrategy.DatabaseOnly.toString();
+
+    @Override
+    public Result audit(APIMessage msg, APIEvent rsp) {
+        return null;
+    }
+
+    public String getImageUuid() {
+        return imageUuid;
+    }
+
+    public void setImageUuid(String imageUuid) {
+        this.imageUuid = imageUuid;
+    }
+
+    public String getImageGroupUuid() {
+        return imageGroupUuid;
+    }
+
+    public void setImageGroupUuid(String imageGroupUuid) {
+        this.imageGroupUuid = imageGroupUuid;
+    }
+
+    public String getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(String strategy) {
+        this.strategy = strategy;
+    }
+}
