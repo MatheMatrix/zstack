92016544f73b312cf3460f78c5786abb60f142d3
yingzhe.hu <yingzhe.hu@zstack.io>
2025-06-05 17:56:22 +0800

    <fix>[image]: add APIQueryImageGroupRefReply sdk
    Resolves: ZSTAC-73119
    
    Change-Id: I79786f61706569766b6d706979627a6a77737767

---
+++ sdk/src/main/java/org/zstack/sdk/ImageMediaType.java
@@ -0,0 +1,7 @@
+package org.zstack.sdk;
+
+public enum ImageMediaType {
+	RootVolumeTemplate,
+	DataVolumeTemplate,
+	ISO,
+}
---
+++ sdk/src/main/java/org/zstack/sdk/ImageStatus.java
@@ -0,0 +1,10 @@
+package org.zstack.sdk;
+
+public enum ImageStatus {
+	Creating,
+	Downloading,
+	Ready,
+	Error,
+	Deleted,
+	Migrating,
+}
