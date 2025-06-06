ea06320a872e19016b612cf644da9c8bd6fac80d
weiqiang.xu <weiqiang.xu@zstack.io>
2025-06-06 16:35:39 +0800

    <fix>[conf]: change sql update to V5.3.20
    DBImpact
    
    Resolves: ZSTAC-73178
    
    Change-Id: I62626c6c616a676f776c737866736465716b7564

--- conf/db/upgrade/V5.3.20__schema.sql
+++ conf/db/upgrade/V5.3.20__schema.sql
@@ -184,3 +184,47 @@ CREATE TABLE IF NOT EXISTS  `zstack`.`OvnControllerVmInstanceVO` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 UPDATE `zstack`.`L2NetworkVO` set vSwitchType='TfL2Network' where type='TfL2Network';
+
+
+CALL ADD_COLUMN('GuestVmScriptEO', 'encodingType', 'VARCHAR(32)', 1, 'PlainText');
+CALL ADD_COLUMN('GuestVmScriptExecutedRecordVO', 'encodingType', 'VARCHAR(32)', 1, 'PlainText');
+DROP VIEW IF EXISTS `zstack`.`GuestVmScriptVO`;
+CREATE VIEW `zstack`.`GuestVmScriptVO` AS SELECT uuid, name, description, platform, encodingType, scriptContent, renderParams, scriptType, scriptTimeout, version, createDate, lastOpDate FROM `zstack`.`GuestVmScriptEO` WHERE deleted IS NULL;
+
+
+CREATE TABLE `zstack`.`ImageGroupVO` (
+                                         `uuid` VARCHAR(32) NOT NULL UNIQUE,
+                                         `name` VARCHAR(255) NOT NULL,
+                                         `description` VARCHAR(2048) DEFAULT NULL,
+                                         `imageCount` int unsigned NOT NULL,
+                                         `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+                                         `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+                                         PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `zstack`.`ImageGroupRefVO` (
+                                            `imageUuid` VARCHAR(32) NOT NULL UNIQUE,
+                                            `imageGroupUuid` VARCHAR(32) NOT NULL,
+                                            `size` bigint(20) unsigned DEFAULT NULL COMMENT 'image size',
+                                            `md5sum` varchar(255) DEFAULT NULL COMMENT 'md5sum of image',
+                                            `name` varchar(255) NOT NULL COMMENT 'image name',
+                                            `description` varchar(2048) DEFAULT NULL COMMENT 'image description',
+                                            `url` varchar(1024) NOT NULL COMMENT 'image url',
+                                            `installUrl` varchar(1024) DEFAULT NULL COMMENT 'url where image installed on secondary storage',
+                                            `mediaType` varchar(32) NOT NULL,
+                                            `format` varchar(32) NOT NULL,
+                                            `system` tinyint(3) unsigned DEFAULT 0,
+                                            `platform` varchar(255) DEFAULT NULL,
+                                            `type` varchar(255) NOT NULL COMMENT 'image type',
+                                            `guestOsType` varchar(255) DEFAULT 'other' COMMENT 'guest os type string',
+                                            `state` varchar(32) NOT NULL COMMENT 'image state',
+                                            `status` varchar(32) NOT NULL COMMENT 'image status',
+                                            `actualSize` bigint(20) unsigned DEFAULT NULL,
+                                            `architecture` varchar(32) DEFAULT NULL,
+                                            `virtio` tinyint(1) DEFAULT 1,
+                                            `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+                                            `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+                                            PRIMARY KEY (`imageUuid`),
+                                            KEY `idxImageGroupRefName` (`name`),
+                                            CONSTRAINT `fkImageGroupRefVOImageGroupVO` FOREIGN KEY (`imageGroupUuid`) REFERENCES `ImageGroupVO` (`uuid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
