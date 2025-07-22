122c2d899a3d611a76e60570a10ba1cfcd941204
weiqiang.xu <weiqiang.xu@zstack.io>
2025-07-22 11:07:16 +0800

    <fix>[conf]: change update schma.sql to 5.4.0
    DBImpact
    
    Resolves: ZSTAC-75818
    
    Change-Id: I76676f6266716c737466727575666a757763656e

--- conf/db/upgrade/V5.4.0__schema.sql
+++ conf/db/upgrade/V5.4.0__schema.sql
@@ -6,3 +6,37 @@ CREATE TABLE  `zstack`.`L3NetworkSequenceNumberVO` (
 ALTER TABLE `zstack`.`L3NetworkEO` ADD COLUMN `internalId` INT(32) unsigned DEFAULT 0;
 DROP VIEW IF EXISTS `zstack`.`L3NetworkVO`;
 CREATE VIEW `zstack`.`L3NetworkVO` AS SELECT uuid, name, internalId, description, state, type, zoneUuid, l2NetworkUuid, `system`, dnsDomain, createDate, lastOpDate, category, ipVersion, enableIPAM, isolated FROM `zstack`.`L3NetworkEO` WHERE deleted IS NULL;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`VolumeCbtBackupRecordVO` (
+    `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+    `taskUuid` varchar(32) NOT NULL,
+    `volumeUuid` varchar(32) NOT NULL,
+    `mode` varchar(255) NOT NULL,
+    `target` varchar(2048) NOT NULL,
+    `scratchNodeName` varchar(255) NOT NULL,
+    `bitmapName` varchar(255) NOT NULL,
+    `lastBitmapName` varchar(255),
+    `lastOpDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS  `zstack`.`ImageGroupVO` (
+    `uuid` VARCHAR(32) NOT NULL UNIQUE,
+    `name` VARCHAR(255) NOT NULL,
+    `status` varchar(32) NOT NULL,
+    `description` VARCHAR(2048) DEFAULT NULL,
+    `imageCount` int unsigned NOT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`ImageGroupRefVO` (
+    `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+    `imageUuid` VARCHAR(32) NOT NULL,
+    `imageGroupUuid` VARCHAR(32) NOT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
