7ac58abea4140cbc952bc187cf1ae80a0473099c
Zhang Wenhao <wenhao.zhang@zstack.io>
2025-08-19 11:52:15 +0800

    <feature>[header]: remove deprecated class ProgressReportService
    * merge ActionProgressInventory to TaskProgressInventory
    * replace ProgressReportService by ActionProgressService
    
    Resolves: ZSV-9610
    
    Change-Id: I7163627376766f6f68666f616f72776e63686863

--- conf/db/upgrade/V4.10.20__schema.sql
+++ conf/db/upgrade/V4.10.20__schema.sql
@@ -20,7 +20,8 @@ DROP VIEW IF EXISTS `zstack`.`HostVO`;
 CREATE VIEW `zstack`.`HostVO` AS SELECT uuid, zoneUuid, clusterUuid, name, description, managementIp, hypervisorType,
 state, status, architecture, nqn, hostname, createDate, lastOpDate FROM `zstack`.`HostEO` WHERE deleted IS NULL;
 
-CREATE TABLE `zstack`.`ActionProgressVO` (
+DROP TABLE IF EXISTS `zstack`.`TaskProgressVO`;
+CREATE TABLE IF NOT EXISTS `zstack`.`TaskProgressVO` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT,
     `apiId` char(32) NOT NULL,
     `content` varchar(255) DEFAULT NULL,
@@ -30,6 +31,7 @@ CREATE TABLE `zstack`.`ActionProgressVO` (
     `currentStep` bigint unsigned DEFAULT 0,
     `totalStep` bigint unsigned DEFAULT 1,
     PRIMARY KEY (`id`),
-    KEY `idxActionProgressVOApiId` (`apiId`)
+    KEY `idxTaskProgressVOApiId` (`apiId`),
+    KEY `idxTaskProgressVOLastOpTime` (`lastOpTime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
