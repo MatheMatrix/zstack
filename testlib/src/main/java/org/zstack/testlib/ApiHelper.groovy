ff1cd8f245bb4fe32ef59ef0d6674c186f619151
hanyu.liang <hanyu.liang@zstack.io>
2025-10-16 16:48:10 +0800

    <fix>[sdk]: support integrity encryption for CIS client Resolves: ZSTAC-78411
    Change-Id: I737578411765547655441897574646678757a7a734578

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -18377,6 +18377,33 @@ abstract class ApiHelper {
     }
 
 
+    def getIntegrityEncryptValue(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetIntegrityEncryptValueAction.class) Closure c) {
+        def a = new org.zstack.sdk.GetIntegrityEncryptValueAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def getInterdependentL3NetworksBackupStorages(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetInterdependentL3NetworksBackupStoragesAction.class) Closure c) {
         def a = new org.zstack.sdk.GetInterdependentL3NetworksBackupStoragesAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
