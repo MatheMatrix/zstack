f6d4af489ded217707f84efdc1f3333a7d19df30
tao.gan <tao.gan@zstack.io>
2025-10-29 21:13:56 +0800

    <fix>[host]: support recording upload url
    add a system tag UPLOAD_URL to record the path for chunked upload.
    
    Resolves: ZSV-9928
    
    Change-Id: I687178756d69646475766875736278636b666976

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMHost.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMHost.java
@@ -6976,6 +6976,7 @@ public class KVMHost extends HostBase implements Host {
                         return;
                     }
 
+                    reply.setDirectUploadPath(rsp.directUploadPath);
                     bus.reply(msg, reply);
                     completion.done();
                 }
