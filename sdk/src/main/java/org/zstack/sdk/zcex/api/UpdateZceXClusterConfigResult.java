5c3c703e8ae874861922a48e51022f9bfb400c8f
tao.gan <tao.gan@zstack.io>
2025-11-24 15:23:03 +0800

    <fix>[zstone]: support reinitialization
    Resolves: ZSV-10485
    
    Change-Id: I677873656c746875756864626f696d6e686b6b6c

--- sdk/src/main/java/org/zstack/sdk/zcex/api/UpdateZceXClusterConfigResult.java
+++ sdk/src/main/java/org/zstack/sdk/zcex/api/UpdateZceXClusterConfigResult.java
@@ -3,5 +3,20 @@ package org.zstack.sdk.zcex.api;
 
 
 public class UpdateZceXClusterConfigResult {
+    private boolean clusterInitSuccess;
+    public void setClusterInitSuccess(boolean clusterInitSuccess) {
+        this.clusterInitSuccess = clusterInitSuccess;
+    }
+    public boolean isClusterInitSuccess() {
+        return clusterInitSuccess;
+    }
+
+    private boolean tokenCreated;
+    public void setTokenCreated(boolean tokenCreated) {
+        this.tokenCreated = tokenCreated;
+    }
+    public boolean isTokenCreated() {
+        return tokenCreated;
+    }
 
 }
