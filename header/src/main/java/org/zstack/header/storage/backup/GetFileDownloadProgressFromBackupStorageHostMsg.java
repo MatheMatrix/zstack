19b4a85a40acfde46beb8ad1b93a1d77621734bb
tao.gan <tao.gan@zstack.io>
2025-12-15 18:00:55 +0800

    <fix>[volume]: support thirdPartySoftwarePackage
    APIImpact
    
    Resolves: ZSV-1
    
    Change-Id: I656979726c736267776c7262716969726874716d

---
+++ header/src/main/java/org/zstack/header/storage/backup/GetFileDownloadProgressFromBackupStorageHostMsg.java
@@ -0,0 +1,43 @@
+package org.zstack.header.storage.backup;
+
+import org.zstack.header.message.NeedReplyMessage;
+
+public class GetFileDownloadProgressFromBackupStorageHostMsg extends NeedReplyMessage implements BackupStorageMessage {
+    private String backupStorageUuid;
+    private String backupStorageHostUuid;
+    private String taskUuid;
+    private String hostname;
+
+    @Override
+    public String getBackupStorageUuid() {
+        return backupStorageUuid;
+    }
+
+    public void setBackupStorageUuid(String backupStorageUuid) {
+        this.backupStorageUuid = backupStorageUuid;
+    }
+
+    public String getBackupStorageHostUuid() {
+        return backupStorageHostUuid;
+    }
+
+    public void setBackupStorageHostUuid(String backupStorageHostUuid) {
+        this.backupStorageHostUuid = backupStorageHostUuid;
+    }
+
+    public String getTaskUuid() {
+        return taskUuid;
+    }
+
+    public void setTaskUuid(String taskUuid) {
+        this.taskUuid = taskUuid;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+}
---
+++ sdk/src/main/java/org/zstack/sdk/softwarePackage/header/UploadAndExecuteSoftwareUpgradePackageResult.java
@@ -0,0 +1,15 @@
+package org.zstack.sdk.softwarePackage.header;
+
+
+import org.zstack.sdk.softwarePackage.header.SoftwarePackageInventory;
+
+public class UploadAndExecuteSoftwareUpgradePackageResult {
+    public SoftwarePackageInventory inventory;
+    public void setInventory(SoftwarePackageInventory inventory) {
+        this.inventory = inventory;
+    }
+    public SoftwarePackageInventory getInventory() {
+        return this.inventory;
+    }
+
+}
