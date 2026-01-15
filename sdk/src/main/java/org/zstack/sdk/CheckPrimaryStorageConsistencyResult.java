afb9506b0fcb0ea77a4f637aed86ca53726e0f58
tao.gan <tao.gan@zstack.io>
2026-01-15 14:06:14 +0800

    <feature>[storage]: register and take over sblk
    APIImpact
    
    Resolves: ZSV-11559
    
    Change-Id: I70637377776e777070676c6a6c616e74786b6667

---
+++ sdk/src/main/java/org/zstack/sdk/CheckPrimaryStorageConsistencyResult.java
@@ -0,0 +1,28 @@
+package org.zstack.sdk;
+
+public class CheckPrimaryStorageConsistencyResult {
+    public boolean consistent;
+    public void setConsistent(boolean consistent) {
+        this.consistent = consistent;
+    }
+    public boolean getConsistent() {
+        return this.consistent;
+    }
+
+    public ConsistencyCheckStatus status;
+    public void setStatus(ConsistencyCheckStatus status) {
+        this.status = status;
+    }
+    public ConsistencyCheckStatus getStatus() {
+        return this.status;
+    }
+
+    public java.lang.String candidateVgUuid;
+    public void setCandidateVgUuid(java.lang.String candidateVgUuid) {
+        this.candidateVgUuid = candidateVgUuid;
+    }
+    public java.lang.String getCandidateVgUuid() {
+        return this.candidateVgUuid;
+    }
+
+}
