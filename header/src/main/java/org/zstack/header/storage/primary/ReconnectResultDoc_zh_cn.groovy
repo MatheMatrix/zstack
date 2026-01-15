d591296c27645c6c01157cc6372e210940a60555
tao.gan <tao.gan@zstack.io>
2026-01-15 14:06:14 +0800

    <feature>[storage]: register and take over sblk
    APIImpact
    
    Resolves: ZSV-10000
    
    Change-Id: I70637377776e777070676c6a6c616e74786b6667

---
+++ header/src/main/java/org/zstack/header/storage/primary/ReconnectResultDoc_zh_cn.groovy
@@ -0,0 +1,21 @@
+package org.zstack.header.storage.primary
+
+
+
+doc {
+
+	title "主存储接管重连结果"
+
+	field {
+		name "name"
+		desc "资源名称"
+		type "String"
+		since "4.7.0"
+	}
+	field {
+		name "ordinal"
+		desc ""
+		type "int"
+		since "4.7.0"
+	}
+}
