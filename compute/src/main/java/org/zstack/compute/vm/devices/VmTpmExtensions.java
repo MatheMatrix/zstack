4d31486f3a6b88a16e734ed20d526c81d3b72bbc
Zhang Wenhao <wenhao.zhang@zstack.io>
2026-02-13 14:59:35 +0800

    <fix>[compute]: introduce NVRAM type volume
    If TPM or secure boot is enabled, MN will prepare nvram volume
    and instantiate volume to host
    
    Resolves: ZSV-11310
    Related: ZSPHER-1
    
    Change-Id: I787672786e6873696c6273647778737364767563

--- compute/src/main/java/org/zstack/compute/vm/devices/VmTpmExtensions.java
+++ compute/src/main/java/org/zstack/compute/vm/devices/VmTpmExtensions.java
@@ -1,12 +1,18 @@
 package org.zstack.compute.vm.devices;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.zstack.compute.vm.BuildVmSpecExtensionPoint;
 import org.zstack.header.vm.CreateVmInstanceMsg;
+import org.zstack.header.vm.DiskAO;
 import org.zstack.header.vm.VmInstanceCreateExtensionPoint;
+import org.zstack.header.vm.VmInstanceSpec;
 import org.zstack.header.vm.VmInstanceVO;
 import org.zstack.header.vm.devices.VmDevicesSpec;
 
-public class VmTpmExtensions implements VmInstanceCreateExtensionPoint {
+import static org.zstack.header.vm.VmInstanceConstant.NVRAM_DEFAULT_SIZE;
+
+public class VmTpmExtensions implements VmInstanceCreateExtensionPoint,
+        BuildVmSpecExtensionPoint {
     @Autowired
     private VmTpmManager vmTpmManager;
 
@@ -23,5 +29,18 @@ public class VmTpmExtensions implements VmInstanceCreateExtensionPoint {
         }
 
         vmTpmManager.persistTpmVO(null, vo.getUuid());
+    }
+
+    @Override
+    public void afterBuildVmSpec(VmInstanceSpec spec) {
+        String vmUuid = spec.getVmInventory().getUuid();
+        if (!vmTpmManager.needRegisterNvram(vmUuid)) {
+            return;
+        }
+
+        DiskAO nvramSpec = new DiskAO();
+        nvramSpec.setSize(NVRAM_DEFAULT_SIZE);
+        nvramSpec.setName("nvram-of-VM-" + vmUuid);
+        spec.setNvRamSpec(nvramSpec);
     }
 }
