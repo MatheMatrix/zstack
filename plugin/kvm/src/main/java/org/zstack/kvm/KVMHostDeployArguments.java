56927a653bf5e698beef627eeb3fff401ae42dfc
yingzhe.hu <yingzhe.hu@zstack.io>
2026-03-11 10:55:36 +0800

    <feature>[kvm]: add libvirt TLS config
    Add libvirt.tls.enabled GlobalConfig and useTls field in MigrateVmCmd to support TLS-encrypted libvirt connections for migration and V2V.
    
    Resolves: ZSTAC-81343
    
    Change-Id: I391fa36c0dd63c25c5d85d102bc3579c8eb3d685
    (cherry picked from commit 3feb9e903e7a617aab0f30e3bbb2a9840a795646)

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMHostDeployArguments.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMHostDeployArguments.java
@@ -38,6 +38,8 @@ public class KVMHostDeployArguments extends SyncTimeRequestedDeployArguments {
     private String restartLibvirtd;
     @SerializedName("extra_packages")
     private String extraPackages;
+    @SerializedName("tls_cert_ips")
+    private String tlsCertIps;
 
     private transient boolean forceRun = false;
 
@@ -132,6 +134,14 @@ public class KVMHostDeployArguments extends SyncTimeRequestedDeployArguments {
 
     public void setExtraPackages(String extraPackages) {
         this.extraPackages = extraPackages;
+    }
+
+    public String getTlsCertIps() {
+        return tlsCertIps;
+    }
+
+    public void setTlsCertIps(String tlsCertIps) {
+        this.tlsCertIps = tlsCertIps;
     }
 
     public String getEnableSpiceTls() {
