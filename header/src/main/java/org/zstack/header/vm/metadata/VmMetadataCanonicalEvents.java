3356ca30d09462506aae251add09a9b091d4a357
tao.gan <tao.gan@zstack.io>
2026-03-12 11:46:54 +0800

    <feature>[vm-metadata]: data model, DTO and serialization
    Resolves: ZSV-11300

---
+++ header/src/main/java/org/zstack/header/vm/metadata/VmMetadataCanonicalEvents.java
@@ -0,0 +1,31 @@
+package org.zstack.header.vm.metadata;
+
+import org.zstack.header.message.NeedJsonSchema;
+
+/**
+ * 虚拟机元数据相关 CanonicalEvent 定义�?
+ *
+ * <p>通过 {@code EventFacade.fire()} 发布，供监控系统和巡检机制消费�?/p>
+ */
+public class VmMetadataCanonicalEvents {
+
+    /**
+     * GC 放弃后的 stale 事件路径�?
+     *
+     * <p>�?{@code UpdateVmInstanceMetadataGC} 超过最大重试次数后发布此事件，
+     * {@code MetadataHealthCheckJob} 监听此事件将 VM 加入优先刷新队列�?/p>
+     */
+    public static final String VM_METADATA_STALE_PATH = "/vm/metadata/stale";
+
+    @NeedJsonSchema
+    public static class MetadataStaleData {
+        public String vmInstanceUuid;
+
+        public MetadataStaleData() {
+        }
+
+        public MetadataStaleData(String vmInstanceUuid) {
+            this.vmInstanceUuid = vmInstanceUuid;
+        }
+    }
+}
