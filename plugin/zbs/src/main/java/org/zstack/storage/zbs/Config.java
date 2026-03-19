0823536db9e4a064061d160a907e46a147b2f240
J M <jin.ma@zstack.io>
2026-03-19 11:25:02 +0800

    <fix>[storage]: desensitize ExternalPrimaryStorage config and addonInfo
    Resolves: ZSTAC-80664
    
    Change-Id: I11e572e7d71d970853302cc33f5843637155a595

--- plugin/zbs/src/main/java/org/zstack/storage/zbs/Config.java
+++ plugin/zbs/src/main/java/org/zstack/storage/zbs/Config.java
@@ -1,5 +1,7 @@
 package org.zstack.storage.zbs;
 
+import org.zstack.header.storage.addon.primary.ExternalPrimaryStorageConfig;
+import org.zstack.utils.gson.JSONObjectUtil;
 
 import java.util.Collections;
 import java.util.List;
@@ -9,7 +11,7 @@ import java.util.stream.Collectors;
  * @author Xingwei Yu
  * @date 2024/4/2 11:13
  */
-public class Config {
+public class Config implements ExternalPrimaryStorageConfig {
     public static class Pool {
         public String logicalName;
         public String aliasName;
@@ -23,9 +25,47 @@ public class Config {
     }
 
     private List<String> mdsUrls;
+
+    @Override
+    public String getIdentity() {
+        return ZbsConstants.IDENTITY;
+    }
+
     private List<Pool> pools;
     private String logicalPoolName;
     private transient List<String> poolNames;
+
+    @Override
+    public ExternalPrimaryStorageConfig desensitize() {
+        Config copy = JSONObjectUtil.toObject(JSONObjectUtil.toJsonString(this), Config.class);
+        if (copy.mdsUrls != null) {
+            copy.mdsUrls = copy.mdsUrls.stream()
+                    .map(Config::desensitizeUrl)
+                    .collect(Collectors.toList());
+        }
+        return copy;
+    }
+
+    /**
+     * Mask the password portion of a mdsUrl.
+     * Format: username:password@hostname[:port][/?mdsPort=...]
+     * Uses indexOf/lastIndexOf consistent with MdsUri parsing logic,
+     * so passwords containing ':' or '@' are correctly handled.
+     */
+    private static String desensitizeUrl(String url) {
+        if (url == null) {
+            return null;
+        }
+        int at = url.lastIndexOf("@");
+        if (at <= 0) {
+            return url;
+        }
+        int colon = url.indexOf(":");
+        if (colon < 0 || colon >= at) {
+            return url;
+        }
+        return url.substring(0, colon) + ":*****" + url.substring(at);
+    }
 
     public List<String> getMdsUrls() {
         return mdsUrls;
