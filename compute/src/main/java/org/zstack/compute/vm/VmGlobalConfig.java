2e57288ffff44519f99c0622df1b8786a125ed3e
tao.gan <tao.gan@zstack.io>
2026-03-23 00:26:42 +0800

    <feature>[vm]: add MetadataImpact
    APIImpact
    
    Resolves: ZSV-11559
    
    Change-Id: I6b6a6378627264646d6a76726762736e77787373

--- compute/src/main/java/org/zstack/compute/vm/VmGlobalConfig.java
+++ compute/src/main/java/org/zstack/compute/vm/VmGlobalConfig.java
@@ -139,4 +139,55 @@ public class VmGlobalConfig {
     @GlobalConfigValidation(validValues = {"true", "false"})
     @BindResourceConfig(value = {VmInstanceVO.class, ClusterVO.class})
     public static GlobalConfig RESET_TPM_AFTER_VM_CLONE = new GlobalConfig(CATEGORY, "reset.tpm.after.vm.clone");
+
+    @GlobalConfigValidation(validValues = {"true", "false"})
+    public static GlobalConfig VM_METADATA_ENABLED = new GlobalConfig(CATEGORY, "vm.metadata.enabled");
+
+    @GlobalConfigValidation()
+    public static GlobalConfig VM_METADATA_LAST_REFRESH_VERSION = new GlobalConfig(CATEGORY, "vm.metadata.lastRefreshVersion");
+
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 100)
+    public static GlobalConfig VM_METADATA_FLUSH_CONCURRENCY = new GlobalConfig(CATEGORY, "vm.metadata.flush.concurrency");
+
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 300)
+    public static GlobalConfig VM_METADATA_FLUSH_POLL_INTERVAL = new GlobalConfig(CATEGORY, "vm.metadata.flush.pollInterval");
+
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 1000)
+    public static GlobalConfig VM_METADATA_FLUSH_BATCH_SIZE = new GlobalConfig(CATEGORY, "vm.metadata.flush.batchSize");
+
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 168)
+    public static GlobalConfig VM_METADATA_CLEANUP_GC_INTERVAL = new GlobalConfig(CATEGORY, "vm.metadata.cleanup.gc.interval");
+
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 100)
+    public static GlobalConfig VM_METADATA_FLUSH_MAX_RETRY = new GlobalConfig(CATEGORY, "vm.metadata.flush.maxRetry");
+
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 120)
+    public static GlobalConfig VM_METADATA_FLUSH_ZOMBIE_CLAIM_THRESHOLD = new GlobalConfig(CATEGORY, "vm.metadata.flush.zombieClaimThreshold");
+
+    // 6h = 21600s, 48h = 172800s
+    @GlobalConfigValidation(numberGreaterThan = 21599, numberLessThan = 172801)
+    public static GlobalConfig VM_METADATA_MAINTENANCE_CONTENT_DRIFT_INTERVAL = new GlobalConfig(CATEGORY, "vm.metadata.maintenance.contentDriftInterval");
+
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 86400)
+    public static GlobalConfig VM_METADATA_MAINTENANCE_STALE_RECOVERY_INTERVAL = new GlobalConfig(CATEGORY, "vm.metadata.maintenance.staleRecoveryInterval");
+
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 1000)
+    public static GlobalConfig VM_METADATA_MAINTENANCE_STALE_RECOVERY_MAX_CYCLES = new GlobalConfig(CATEGORY, "vm.metadata.maintenance.staleRecoveryMaxCycles");
+
+    @GlobalConfigValidation(numberGreaterThan = 0)
+    public static GlobalConfig VM_METADATA_PAYLOAD_REJECT_THRESHOLD = new GlobalConfig(CATEGORY, "vm.metadata.payload.rejectThreshold");
+
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 86400)
+    public static GlobalConfig VM_METADATA_MAINTENANCE_ORPHAN_CHECK_INTERVAL = new GlobalConfig(CATEGORY, "vm.metadata.maintenance.orphanCheckInterval");
+
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 1000)
+    public static GlobalConfig VM_METADATA_MAINTENANCE_STALE_RECOVERY_BATCH_SIZE = new GlobalConfig(CATEGORY, "vm.metadata.maintenance.staleRecoveryBatchSize");
+
+    // Range: 10-30
+    @GlobalConfigValidation(numberGreaterThan = 9, numberLessThan = 31)
+    public static GlobalConfig VM_METADATA_MAINTENANCE_CONTENT_DRIFT_BATCH_SIZE = new GlobalConfig(CATEGORY, "vm.metadata.maintenance.contentDriftBatchSize");
+
+    // Range: 1-10 seconds
+    @GlobalConfigValidation(numberGreaterThan = 0, numberLessThan = 11)
+    public static GlobalConfig VM_METADATA_MAINTENANCE_CONTENT_DRIFT_BATCH_SLEEP_SEC = new GlobalConfig(CATEGORY, "vm.metadata.maintenance.contentDriftBatchSleepSec");
 }
