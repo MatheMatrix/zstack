ef89fe1d111910b283d05e869d54adf18b70a323
shixin.ruan <shixin.ruan@zstack.io>
2026-05-12 14:02:52 +0800

    <fix>[zns]: show geneve network vni
    Resolves: ZCF-3472
    
    Change-Id: I6e2f6ae5c2a9c6a2e7fbaa9a7e4accfd33edd6b5

--- sdk/src/main/java/org/zstack/sdk/network/zns/L2GeneveNetworkInventory.java
+++ sdk/src/main/java/org/zstack/sdk/network/zns/L2GeneveNetworkInventory.java
@@ -7,9 +7,20 @@ public class L2GeneveNetworkInventory extends org.zstack.sdk.L2NetworkInventory 
     public java.lang.Integer geneveId;
     public void setGeneveId(java.lang.Integer geneveId) {
         this.geneveId = geneveId;
+        if (this.vni == null) {
+            this.vni = geneveId;
+        }
     }
     public java.lang.Integer getGeneveId() {
         return this.geneveId;
+    }
+
+    public java.lang.Integer vni;
+    public void setVni(java.lang.Integer vni) {
+        this.vni = vni;
+    }
+    public java.lang.Integer getVni() {
+        return this.vni;
     }
 
 }
