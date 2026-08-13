347db63b5ca64482bcd57c2712bf1c824f741ab8
shixin.ruan <shixin@shixin.ruan>
2026-08-13 20:15:31 +0900

    <fix>[zns]: sync segment API helpers
    Regenerate ZNS API helpers and L2 inventory documentation.
    
    Resolves: ZCF-5485
    
    Change-Id: I739f13fbcf5c8472c89dfc7ea2fea6fa12eb84c9

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -53322,6 +53322,32 @@ abstract class ApiHelper {
                 a.apiId = Platform.uuid
             }
 
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
+    def getZnsSegmentSyncStatus(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.network.zns.GetZnsSegmentSyncStatusAction.class) Closure c) {
+        def a = new org.zstack.sdk.network.zns.GetZnsSegmentSyncStatusAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
 
             def tracker = new ApiPathTracker(a.apiId)
             def out = errorOut(a.call())
@@ -53330,6 +53356,34 @@ abstract class ApiHelper {
                 Test.apiPaths[a.class.name] = path.join(" --->\n")
             }
 
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
+    def queryZnsSegmentRef(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.network.zns.QueryZnsSegmentRefAction.class) Closure c) {
+        def a = new org.zstack.sdk.network.zns.QueryZnsSegmentRefAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+
+        a.conditions = a.conditions.collect { it.toString() }
+
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
 
             return out
         } else {
@@ -53389,6 +53443,33 @@ abstract class ApiHelper {
                 Test.apiPaths[a.class.name] = path.join(" --->\n")
             }
         
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
+    def useZnsSegment(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.network.zns.UseZnsSegmentAction.class) Closure c) {
+        def a = new org.zstack.sdk.network.zns.UseZnsSegmentAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+
             return out
         } else {
             return errorOut(a.call())
